8a25551668dabc7ae7156ec272d167ab39f14658
L D <tapircode@gmail.com>
2019-04-10 05:25:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,10 +71,11 @@ def all_supplies_in_holidays(holiday_hash)
     puts new_season
     new_line = "  "
     holiday.each do |name, supply|
+      new_line += name.slice(0,1).capitalize + name.slice(1..-1) + ": "
       if supply.length == 1
-          new_line += name.slice(0,1).capitalize + name.slice(1..-1)
+          new_line += supply[0].slice(0,1).capitalize + supply[0].slice(1..-1)
       else
-        new_line += supply[0].slice(0,1).capitalize + supply[0].slice(1..-1) +": "
+        new_line += supply[0].slice(0,1).capitalize + supply[0].slice(1..-1)
         (1...supply.length).each do |i|
           new_line += ", " + supply[i].slice(0,1).capitalize + supply[i].slice(1..-1)
         end  
